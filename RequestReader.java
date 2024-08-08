dc993a7892c354250765823ee8f20aff7b2795a7
BdAbiy <badr22hari@gmail.com>
2024-08-08 22:25:08 +0200

    update

--- RequestReader.java
+++ RequestReader.java
@@ -1,10 +1,10 @@
 public class RequestReader {
     public static void main(String[] args) {
-        ReadRequest("GET /users/romm HTTP/1.1");
+        ReadRequest("POST var HTTP/1.1");
     }
     public static String[] ReadRequest(String r){
         boolean ok = false;
-        String[] resulte = {"",""};
+        String[] resulte = {"","",""};
         int size =0;
         if (r.endsWith("HTTP/1.0") ||r.endsWith("HTTP/1.1")){size = r.length()-8;}else if (r.endsWith("HTTP/3") ||r.endsWith("HTTP/2")){size = r.length()-6;}
         
@@ -19,29 +19,23 @@ public class RequestReader {
                 if (requestchar[j] == ' '){startpos=j+1;entersjloop = true;break;}
                 if (i == 0){methode += requestchar[j];}else if (i == 1){path += requestchar[j];}
             }
+            resulte[0] = methode;
+            resulte[2] = path;
         }System.err.println("methode :"+ methode + "\npath:" + path);
-        if (r.startsWith("GET")){
+       
              if (r.contains("script")|| r.contains("js")){
-                resulte[0] = "Gscript";
-                for (int i = 4; i != size; i++) {
-                    resulte[1] += requestchar[i];
-                }
+                resulte[1] = "script";
+                
             }else if (r.contains("style")|| r.contains("css")){
-                resulte[0] = "Gstyle";
-                for (int i = 4; i != size; i++) {
-                    resulte[1] += requestchar[i];
-                }
+                resulte[1] = "style";
+               
             }else if (r.contains("png") || r.contains("jpg") || r.contains("jpeg")){
-                resulte[0] ="Gimg";
-                for (int i = 4; i != size; i++) {
-                    resulte[1] += requestchar[i];
-                }
+                resulte[1] ="img";
+                
             }else if (r.contains("ico")){
-                resulte[0] ="Gico";
-                for (int i = 4; i != size; i++) {
-                    resulte[1] += requestchar[i];
-                }
-            }
+                resulte[1] ="ico";
+                
+            }else{resulte[1]="indexfile";}
             try {
                 
             } catch (Exception e) {
@@ -50,9 +44,7 @@ public class RequestReader {
             if (resulte[1].isEmpty()){
                 resulte[0] = "redirect";
             }
-        }else if (r.startsWith("POST")){
-            
-        }
+       
         return resulte;
-    }
-}
+        }}
+
